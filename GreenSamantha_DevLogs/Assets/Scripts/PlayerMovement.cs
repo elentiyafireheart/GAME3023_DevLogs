a214a45eb72cbb20e92ee32c56f50c15eefab3a0
Samantha Green <115169809+elentiyafireheart@users.noreply.github.com>
2024-09-13 23:38:32 -0400

    Added Animations to Character Movement (FINAL COMMIT)

--- GreenSamantha_DevLogs/Assets/Scripts/PlayerMovement.cs
+++ GreenSamantha_DevLogs/Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float speed = 2.0f;
+    public float _speed = 2.0f; // increase speed
+    public bool _isWalking;
+    public int _walkDirection;
+    [SerializeField] private Animator _animator; // exposes the animator
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +22,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // horizontal parameter
+        _animator.SetFloat("Horizontal",Input.GetAxis("Horizontal"));
+        _animator.SetFloat("Vertical",Input.GetAxis("Vertical"));
+
         // Gives back value depending on what arrow key you press (left/right movement)
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        Vector3 vertical = new Vector3(0.0f,Input.GetAxis("Vertical"), 0.0f);
         // y and z-axis stays the same since we're moving it in 2D
         // moving sprite on x-axis
-        transform.position = transform.position + horizontal* Time.deltaTime * speed;
+        transform.position = transform.position + horizontal* Time.deltaTime * _speed;
+        transform.position = transform.position + vertical * Time.deltaTime * _speed;
         // multiplying by deltaTime helps smooth out the movement
         // multiply by speed to make it move faster
     }
