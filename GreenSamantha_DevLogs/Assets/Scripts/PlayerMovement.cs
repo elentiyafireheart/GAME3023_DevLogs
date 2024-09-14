fcd7d3d3a0a9318d40861feb4c56ded1cf71954a
Samantha Green <115169809+elentiyafireheart@users.noreply.github.com>
2024-09-13 22:04:06 -0400

    Added Character Movement to Player/Sprite

--- GreenSamantha_DevLogs/Assets/Scripts/PlayerMovement.cs
+++ GreenSamantha_DevLogs/Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// sprite sheet from https://pipoya.itch.io/pipoya-free-rpg-character-sprites-32x32
+
+
 public class PlayerMovement : MonoBehaviour
 {
 
-
+    public float speed = 2.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +19,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Gives back value depending on what arrow key you press (left/right movement)
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        // y and z-axis stays the same since we're moving it in 2D
+        // moving sprite on x-axis
+        transform.position = transform.position + horizontal* Time.deltaTime * speed;
+        // multiplying by deltaTime helps smooth out the movement
+        // multiply by speed to make it move faster
     }
 }
