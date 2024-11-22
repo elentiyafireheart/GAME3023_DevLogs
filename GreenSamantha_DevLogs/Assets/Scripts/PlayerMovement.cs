8c5ad9baeec429b7ea8f4f9d9fd267c3e5c85d6e
Migel Mata <migel.mata@georgebrown.ca>
2024-11-22 16:00:29 -0500

    Added UI elements to display pick up prompt and current inventory space

--- GreenSamantha_DevLogs/Assets/Scripts/PlayerMovement.cs
+++ GreenSamantha_DevLogs/Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 // sprite sheet from https://pipoya.itch.io/pipoya-free-rpg-character-sprites-32x32
+
+// item sprites from https://opengameart.org/node/113951
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,37 +14,47 @@ public class PlayerMovement : MonoBehaviour
     public bool _isWalking;
     public int _walkDirection;
 
-    public LayerMask grassLayer;
-    public LayerMask specialGrassLayer;
-
     public bool hasEncountered;
     [SerializeField]
     private Animator _animator; // exposes the animator
     public GameObject Camera_1;
     public GameObject Camera_2;
     public GameObject Ability_3;
-    public float playerLevel = 1.0f;
+
+    // LAYERS
+    public LayerMask grassLayer;
+    public LayerMask specialGrassLayer;
+
+    // PLAYER
     [SerializeField]
-    public float playerHealth = 100;
+    public float playerHealth;
+    public float playerLevel = 1.0f;
     public int playerInventory = 0;
     public int bluePotionsInventory = 0;
     public int greenPotionsInventory = 0;
     public int redPotionsInventory = 0;
-
     [SerializeField]
     public int playerMaxInventory;
 
+    // ENEMY
     public GameObject _enemyTemplatePrefab;
     public GameObject _enemySpecialPrefab;
-
     GameObject templateEnemyObj;
     GameObject specialEnemyObj;
+
+    // UI
+    public TMP_Text _promptText;
+    public TMP_Text _infoText;
+    public TMP_Text _inventoryText;
 
     // Start is called before the first frame update
     void Start()
     {
         Cam_1();
         Ability_3.SetActive(false);
+
+        _promptText.enabled = false;
+        _infoText.enabled = false;
     }
 
     // Update is called once per frame
@@ -67,6 +80,17 @@ public class PlayerMovement : MonoBehaviour
         {
             CheckForEncounters();
         }
+
+        // Reveal the inventory UI element if the player has something in inventory
+        if (playerInventory > 0)
+        {
+            _inventoryText.enabled = true;
+        }
+        else
+        {
+            _inventoryText.enabled = false;
+        }
+        _inventoryText.text = "Inventory: " + playerInventory + "/" + playerMaxInventory;
     }
 
     void Cam_1()
@@ -132,29 +156,49 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    public void OnCollisionEnter2D(Collision2D collisionInfo)
+    public void OnCollisionStay2D(Collision2D collisionInfo)
     {
         if (collisionInfo.collider.tag == "BluePotionItem")
         {
-            Destroy(collisionInfo.gameObject);
-            Debug.Log("Collected 1 BLUE POTION");
-            playerInventory += 1;
-            bluePotionsInventory += 1;
+            _promptText.enabled = true;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Collected 1 BLUE POTION");
+                Destroy(collisionInfo.gameObject);
+                playerInventory += 1;
+                bluePotionsInventory += 1;
+                _promptText.enabled = false;
+            }
         }
         if (collisionInfo.collider.tag == "GreenPotionItem")
         {
-            Destroy(collisionInfo.gameObject);
-            Debug.Log("Collected 1 GREEN POTION");
-            playerInventory += 1;
-            greenPotionsInventory += 1;
+            _promptText.enabled = true;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Collected 1 GREEN POTION");
+                Destroy(collisionInfo.gameObject);
+                playerInventory += 1;
+                greenPotionsInventory += 1;
+                _promptText.enabled = false;
+            }
         }
         if (collisionInfo.collider.tag == "RedPotionItem")
         {
-            Destroy(collisionInfo.gameObject);
-            Debug.Log("Collected 1 RED POTION");
-            playerInventory += 1;
-            redPotionsInventory += 1;
+            _promptText.enabled = true;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Collected 1 RED POTION");
+                Destroy(collisionInfo.gameObject);
+                playerInventory += 1;
+                redPotionsInventory += 1;
+                _promptText.enabled = false;
+            }
         }
+    }
+
+    public void OnCollisionExit2D(Collision2D collisionInfo)
+    {
+        _promptText.enabled = false;
     }
 
     public void FleeBattle()
